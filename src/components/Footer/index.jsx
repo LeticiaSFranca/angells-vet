f68ceac894a6b32c807bacdd3c9a868ef8cb0a3f
LeticiaSFranca <leticiasilvafranca17@gmail.com>
2024-05-28 10:13:59 -0300

    Update | Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,8 @@ import './Footer.style.css';
 export function Footer() {
     return (
         <footer>
-            <p>Desenvolvido por <a href="https://leticiafranca.com.br" target="_blank" rel="noopener noreferrer">Letícia França</a> <br />Projeto de uma Unidade Curricular da Faculdade</p>
+            <p>Desenvolvido por <a href="https://leticiafranca.com.br" target="_blank" rel="noopener noreferrer">Letícia França</a> <br />
+            All rights reserverd © by <a href="https://gf-company.vercel.app/" target="_blank" rel="noopener noreferrer">GF Company</a></p>
         </footer>
     );
 }
